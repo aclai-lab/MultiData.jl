c331dbfd92c1333879e50db1fb39026032a4ffdf
giopaglia <24519853+giopaglia@users.noreply.github.com>
2024-02-02 14:55:37 +0100

    Fix tests

--- test/dimensional-data.jl
+++ test/dimensional-data.jl
@@ -7,7 +7,7 @@ for ((channel_size1, channel_size2), shouldfail) in [
     ((1,),()) => true,
     ((1,2),()) => true,
 ]
-    df = DataFrame(
+    local df = DataFrame(
         x=[rand(channel_size1...) for i_instance in 1:_ninstances],
         y=[rand(channel_size2...) for i_instance in 1:_ninstances]
     )
@@ -19,15 +19,22 @@ for ((channel_size1, channel_size2), shouldfail) in [
     end
 end
 
+begin
+    local df = MultiData.dimensional2dataframe(eachslice(rand(3,4); dims=2), ["a", "b", "c"])
+    @test first(unique(size.(MultiData.dataframe2dimensional(df)[1]))) == (3,)
+end
 
-df = MultiData.dimensional2dataframe(eachslice(rand(3,4); dims=2), ["a", "b", "c"])
-@test first(unique(size.(MultiData.dataframe2dimensional(df)[1]))) == (3,)
+begin
+    local df = MultiData.dimensional2dataframe(eachslice(rand(1,3,4); dims=3), ["a", "b", "c"])
+    @test first(unique(size.(MultiData.dataframe2dimensional(df)[1]))) == (1,3)
+end
 
-df = MultiData.dimensional2dataframe(eachslice(rand(1,3,4); dims=3), ["a", "b", "c"])
-@test first(unique(size.(MultiData.dataframe2dimensional(df)[1]))) == (1,3)
+begin
+    local df = MultiData.dimensional2dataframe(eachslice(rand(2,3,4); dims=3), ["a", "b", "c"])
+    @test first(unique(size.(MultiData.dataframe2dimensional(df)[1]))) == (2,3)
+end
 
-df = MultiData.dimensional2dataframe(eachslice(rand(2,3,4); dims=3), ["a", "b", "c"])
-@test first(unique(size.(MultiData.dataframe2dimensional(df)[1]))) == (2,3)
-
-df = MultiData.dimensional2dataframe(eachslice(rand(2,2,3,4); dims=4), ["a", "b", "c"])
-@test first(unique(size.(MultiData.dataframe2dimensional(df)[1]))) == (2,2,3)
+begin
+    local df = MultiData.dimensional2dataframe(eachslice(rand(2,2,3,4); dims=4), ["a", "b", "c"])
+    @test first(unique(size.(MultiData.dataframe2dimensional(df)[1]))) == (2,2,3)
+end
